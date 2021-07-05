5a47836731f00a9b96b8fec9805b34c4b665910a
TadasDaugalas <86734825+TadasDaugalas@users.noreply.github.com>
2021-07-05 18:36:55 +0300

    Update PirmasDarbas.java

--- PirmasDarbas.java
+++ PirmasDarbas.java
@@ -2,5 +2,6 @@ public class PirmasDarbas {
     public static void main(String[] args) {
         System.out.println("Sveiki Mano vardas Tadas");
         System.out.println("Gyvenu vilniuje");
+        System.out.println("Gyvenu vilniuje");
     }
 }
